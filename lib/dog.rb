84153f849a75b84f83a667e57d7289580a16d96d
Peter K <peteryunkim@gmail.com>
2017-03-14 09:42:04 -0400

    done

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog
+	#code block here
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+	#code here
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
